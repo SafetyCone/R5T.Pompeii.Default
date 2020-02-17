10c395e4cda119731a47e042060287fd1c248918
MinexAutomation <minex.automation@gmail.com>
2020-02-16 22:36:48 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Pompeii.Default.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Pompeii.Default.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Pompeii.Default/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Pompeii.Default
+{
+    public class Class1
+    {
+    }
+}
