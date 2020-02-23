ed368c212cc749deed894d22443299771944600e
MinexAutomation <minex.automation@gmail.com>
2020-02-23 12:16:22 -0800

    Added documentation.

--- source/R5T.Pompeii.Default/Code/Services/Implementations/PublishDirectoryProjectBuildOutputBinariesDirectoryPathProvider.cs
+++ source/R5T.Pompeii.Default/Code/Services/Implementations/PublishDirectoryProjectBuildOutputBinariesDirectoryPathProvider.cs
@@ -3,6 +3,10 @@
 
 namespace R5T.Pompeii.Default
 {
+    /// <summary>
+    /// Gets the binaries directory path from the solution file path assuming the standard development directory structure enforced by Visual Studio:
+    ///     ../{Solution Directory}/{Project Directory}/bin/Debug/netcoreapp2.2/publish (the binaries directory)
+    /// </summary>
     public class PublishDirectoryProjectBuildOutputBinariesDirectoryPathProvider : IProjectBuildOutputBinariesDirectoryPathProvider
     {
         private IEntryPointProjectBuildOutputPublishDirectoryPathProvider EntryPointProjectBuildOutputPublishDirectoryPathProvider { get; }
